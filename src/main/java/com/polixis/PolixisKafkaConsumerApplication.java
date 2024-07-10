7f02d671d5479ac666cafe1712a5b80642ea772f
VardanTevosyan <avtevosyan@gmail.com>
2024-07-10 21:19:55 +0400

    Polixis project setup.

---
+++ src/main/java/com/polixis/PolixisKafkaConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.polixis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolixisKafkaConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolixisKafkaConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/polixis/PolixisKafkaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polixis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolixisKafkaConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
